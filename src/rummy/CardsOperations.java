9c820033120249c18f9560e7c8af0d55490072a4
Ekansh Mittal <test@L-MAA-25004063.paypalcorp.com>
2015-07-30 17:58:07 +0530

    added Card Operations class

---
+++ src/rummy/CardsOperations.java
@@ -0,0 +1,29 @@
+package rummy;
+
+import java.util.List;
+
+public class CardsOperations {
+	public CardsOperations() {
+
+	}
+
+	public boolean isSequence(List<Card> card) {
+		return true;
+	}
+
+	public boolean isNaturalSequence(List<Card> card) {
+		return true;
+	}
+
+	public boolean isTriplet(List<Card> card) {
+		return true;
+	}
+
+	public boolean isQuad(List<Card> card) {
+		return true;
+	}
+
+	public boolean isSequenceOfFive(List<Card> card) {
+		return true;
+	}
+}
