5dae73311464f32b3cd3b1463b7be20279b121b3
Ekansh Mittal <test@L-MAA-25004063.paypalcorp.com>
2015-07-30 17:39:09 +0530

    random function to get 13 cards

--- src/rummy/Rummy.java
+++ src/rummy/Rummy.java
@@ -1,5 +1,24 @@
 package rummy;
 
 public class Rummy {
+	private String[] allRanks = { "Ace", "1", "2", "3", "4", "5", "6", "7", "8",
+			"9", "10", "Joker", "Queen", "King" };
+	private String[] allSuits = { "heart", "spade", "diamond", "club" };
 
+	public Rummy() {
+
+	}
+
+	public static void main(String[] args) {
+		Rummy rummy = new Rummy();
+		for (int i = 0; i < 13; i++) {
+			int rank = rummy.generateRandomNumber(13);
+			int suit = rummy.generateRandomNumber(4);
+			System.out.println(rummy.allRanks[rank] + " " + rummy.allSuits[suit]);
+		}
+	}
+
+	private int generateRandomNumber(int maximum) {
+		return 0 + (int) (Math.random() * maximum);
+	}
 }
